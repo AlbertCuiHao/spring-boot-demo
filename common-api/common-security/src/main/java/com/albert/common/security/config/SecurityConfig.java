54b53c53f8a74f00971d91bcbab9e371d447c26b
albert <albert.cui.hao@gmail.com>
2023-02-15 09:20:12 +0800

    整合security

--- common-api/common-security/src/main/java/com/albert/common/security/config/SecurityConfig.java
+++ common-api/common-security/src/main/java/com/albert/common/security/config/SecurityConfig.java
@@ -1,86 +1,68 @@
 package com.albert.common.security.config;
 
-import com.albert.common.security.filter.AuthenticationFilter;
-import com.albert.common.security.filter.OncePreAuthenticationFilter;
-import com.albert.common.security.handler.AuthenticationLogout;
-import com.albert.common.security.handler.Http401AuthenticationEntryPoint;
-import com.albert.common.security.serivce.UserServiceImpl;
+
+import com.albert.common.security.filter.MyOncePerRequestFilter;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.annotation.Resource;
-
-@MapperScan({"com.albert.common.security.mapper"})
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Resource
-    private RedisTemplate<String, Object> redisTemplate;
+@EnableWebSecurity
+@EnableMethodSecurity
+@MapperScan({"com.albert.common.security.mapper"})
+@ComponentScan({"com.albert.common.security.filter", "com.albert.common.security.handler"})
+public class SecurityConfig {
+
+    AuthenticationConfiguration authenticationConfiguration;
+
+    @Autowired
+    public void setAuthenticationConfiguration(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        return new UserServiceImpl();
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity httpSecurity,
+            AuthenticationSuccessHandler authenticationSuccessHandler,
+            AuthenticationFailureHandler authenticationFailureHandler,
+            AuthenticationEntryPoint authenticationEntryPoint,
+            LogoutSuccessHandler logoutSuccessHandler) throws Exception {
+        httpSecurity.formLogin().usernameParameter("username").passwordParameter("password")
+                .loginProcessingUrl("/login")
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+                .and().authorizeHttpRequests()
+                .anyRequest().authenticated().and().authenticationManager(authenticationManager(authenticationConfiguration))
+                .addFilterBefore(new MyOncePerRequestFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+                .and().logout().logoutUrl("/logout").logoutSuccessHandler(logoutSuccessHandler);
+        httpSecurity.csrf().disable().cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return httpSecurity.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
-    }
-
-    private final String[] permitList = new String[]{
-            "/login",
-            "/swagger**/**",
-            "/webjars/**",
-            "/v3/**",
-    };
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http       //允许跨域
-                .cors().and()
-                //关闭 csrf
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//去除默认session
-                .and().authorizeRequests()
-                // 不需要登录的请求
-                .antMatchers(permitList).permitAll()
-                // .antMatchers("/admin").hasAnyRole("ROLE_admin")
-                //任何请求方式
-                .anyRequest().authenticated()
-                .and().exceptionHandling().authenticationEntryPoint(new Http401AuthenticationEntryPoint())
-                .and().addFilter(new AuthenticationFilter(authenticationManager(), redisTemplate))
-                .addFilter(new OncePreAuthenticationFilter(authenticationManager(), redisTemplate))
-                .logout()
-                .logoutSuccessHandler(new AuthenticationLogout(redisTemplate))
-                .permitAll();
-    }
-
-    /**
-     * 跨域
-     */
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
 }
