2d78b9c69e9377823d4528e284a82eec4f265ac4
albert <albert.cui.hao@gmail.com>
2023-02-15 19:41:36 +0800

    redis

--- common-api/common-redis/src/main/java/com/albert/common/redis/config/RedisConfig.java
+++ common-api/common-redis/src/main/java/com/albert/common/redis/config/RedisConfig.java
@@ -13,7 +13,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Bean(name = "redisTemplate")
+    @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
