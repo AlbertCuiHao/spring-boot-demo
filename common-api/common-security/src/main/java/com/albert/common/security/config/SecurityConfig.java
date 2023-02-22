1a19df31caf467b8844cacf77965f57c6b8ed5c3
albert <albert.cui.hao@gmail.com>
2023-02-22 12:17:07 +0800

    跨域问题

--- common-api/common-security/src/main/java/com/albert/common/security/config/SecurityConfig.java
+++ common-api/common-security/src/main/java/com/albert/common/security/config/SecurityConfig.java
@@ -20,6 +20,12 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -57,7 +63,7 @@ public class SecurityConfig {
                 .loginProcessingUrl("/login")
                 .successHandler(authenticationSuccessHandler)
                 .failureHandler(authenticationFailureHandler)
-                .and().authorizeHttpRequests()
+                .and().authorizeHttpRequests().requestMatchers("/swagger-resources/**", "/swagger-ui/**", "/v3/**", "/error").permitAll()
                 .anyRequest().authenticated().and().authenticationManager(authenticationManager(authenticationConfiguration))
                 .addFilterBefore(oncePerRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
@@ -67,4 +73,15 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin", "X-Requested-With", "Origin"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
